d1f65b6b09d60574cf37b93a7dd5f2f02385527e
TerryZ <terry5@foxmail.com>
2024-08-08 16:06:00 +0800

    feat: 全屏预览布局组件

---
+++ examples/FullscreenPreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <Teleport to="body">
+    <div class="position-fixed top-0 start-0 bg-white">
+      <slot />
+      <div
+        class="
+        shadow bg-white rounded-circle
+        d-flex justify-content-center align-items-center
+        position-fixed
+      "
+        style="width: 60px;height: 60px;bottom: 2rem;right:2rem;"
+        @click="emit('exit')"
+      >
+        x
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+const emit = defineEmits(['exit'])
+</script>
