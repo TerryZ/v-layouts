218dcb9bec30feb3fd1c9849b4a4b38134364aa3
TerryZ <terry5@foxmail.com>
2024-08-21 17:47:34 +0800

    feat: remove export panel-switcher

--- src/modules/panel-group/index.js
+++ src/modules/panel-group/index.js
@@ -1,9 +1,7 @@
 import PanelGroup from './PanelGroup.vue'
 import PanelItem from './PanelItem.vue'
-import PanelSwitcher from './PanelSwitcher.vue'
 
 export {
   PanelGroup,
-  PanelItem,
-  PanelSwitcher
+  PanelItem
 }
